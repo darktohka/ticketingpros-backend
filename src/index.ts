9790a52a736f49f90e3364bee717b5a5f7708b3a
Disyer <daniel@tohka.us>
2025-03-14 16:08:38 +0200

    Implement CORS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { getAllTickets } from './ticket-service';
 
 const app = new Hono();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN as string,
+  }),
+);
 
 app.get('/tickets', async (c) => {
   const tickets = await getAllTickets();
